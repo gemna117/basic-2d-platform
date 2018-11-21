5338f38cab2f489be430dbb8468de337f2d12da1
megan alonzo <megan.alonzo@loop.colum.edu>
2018-11-21 11:06:14 -0600

    platformer fall script

---
+++ basic 2d platform/Assets/Scripts/PlatformerFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerFall : MonoBehaviour {
+
+    public float fallDelay;
+
+    private Rigidbody2D rb2d;
+	// Use this for initialization
+	void awake ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("player"))
+            rb2d.isKinematic = false;
+    }
+}
